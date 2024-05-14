df3a69c6dd842f193fab11954bca7c77e74e4acd
Ethan Muller <ethanbmuller@gmail.com>
2024-05-14 19:02:22 -0400

    dumber camera

--- src/game/CameraMovement.ts
+++ src/game/CameraMovement.ts
@@ -12,60 +12,20 @@ export class CameraMovement
     }
 
     currentPlayerTile : Vector2 = new Vector2();
-    lookAtTile : Vector2 = new Vector2();
     lookAtPosition : Vector3 = new Vector3();
-    currentTile : Vector2 = new Vector2();
 
-    wantedLookAtTile : Vector2 = new Vector2();
-    wantedTile : Vector2 = new Vector2();
-    wantedPosition : Vector3 = new Vector3();
-    wantedLookAtPosition : Vector3 = new Vector3();
-    lerping = false;
+    playerTop(time: Time, player: Mouse, level: Level) {
+      this.camera.position.copy(player.object.position)
+      this.camera.position.y += 10
+      this.camera.position.z += 10
 
-    lookAtPlayer = false; // If false, we will look at the center of the tile we are facing
-
-    cameraSpeed = 100;
+      this.lookAtPosition.copy(player.object.position);
+      this.camera.lookAt(this.lookAtPosition);
+      this.camera.updateProjectionMatrix();
+    }
 
     update(time: Time, player: Mouse, level: Level)
     {
-        level.getTileFromWorldPosition(player.object.position, this.currentPlayerTile);
-
-        if (this.currentPlayerTile.x != this.lookAtTile.x || this.currentPlayerTile.y != this.lookAtTile.y)
-        {
-            this.wantedLookAtTile.copy(this.currentPlayerTile);
-            this.wantedTile.copy(this.lookAtTile);
-        }
-
-        if (this.wantedTile.x != this.currentTile.x || this.wantedTile.y != this.currentTile.y || this.wantedLookAtTile.x != this.lookAtTile.x || this.wantedLookAtTile.y != this.lookAtTile.y)
-        {
-            this.wantedTile.copy(this.wantedTile);
-            this.lookAtTile.copy(this.wantedLookAtTile);
-            level.getWorldPositionFromTile(this.wantedTile, this.wantedPosition);
-            this.wantedPosition.y += level.wallHeight * 0.5;
-            level.getWorldPositionFromTile(this.lookAtTile, this.wantedLookAtPosition);
-
-            this.lerping = true;
-        }
-
-        if (this.lerping) {
-
-            let maxFrameDisplacement = time.deltaTime * this.cameraSpeed;
-            this.lerping = Utils.MoveTowards(this.camera.position, this.wantedPosition, maxFrameDisplacement);
-
-            if (!this.lerping) {
-                this.lookAtPosition.copy(this.wantedLookAtPosition);
-            }
-            else {
-                Utils.MoveTowards(this.lookAtPosition, this.wantedLookAtPosition, maxFrameDisplacement);
-            }
-            if (!this.lookAtPlayer) {
-                this.camera.lookAt(this.lookAtPosition);
-                this.camera.updateProjectionMatrix();
-            }
-        }
-        if (this.lookAtPlayer) {
-            this.camera.lookAt(player.object.position);
-            this.camera.updateProjectionMatrix();
-        }
+      this.playerTop(time, player, level)
     }
 }
